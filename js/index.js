3d30b5ea3a9676235b5be0bed979cd812df50c75
R00T80Y <86746744+R00T80Y@users.noreply.github.com>
2022-02-09 16:32:22 +0300

    IE11

--- js/index.js
+++ js/index.js
@@ -3,15 +3,15 @@ const $buttonEmulated = ToggleButton(document.querySelectorAll('.js-button-emula
   // Plugin options here
 });
 }
-document.getElementById('burger-toggle-new').addEventListener('change', function(event) {
-  event.preventDefault() && event.stopPropagation();
+// document.getElementById('burger-toggle-new').addEventListener('change', function(event) {
+//   event.preventDefault() && event.stopPropagation();
 
-  console.log('Custom Change');
-});
+//   console.log('Custom Change');
+// });
 
-for (let i = 0, l = $buttonEmulated.length; i < l; ++i) {
-  console.log($buttonEmulated[i]);
-}
+// for (let i = 0, l = $buttonEmulated.length; i < l; ++i) {
+//   console.log($buttonEmulated[i]);
+// }
 
 // for (let i = 0, l = $buttonEmulated.length; i < l; ++i) {
 //   $buttonEmulated[i].destroy();
