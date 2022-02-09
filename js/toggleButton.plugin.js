8ae4f4c13c6976c4e7ec2ee16af987196228f59f
R00T80Y <86746744+R00T80Y@users.noreply.github.com>
2022-02-09 16:43:01 +0300

    Update toggleButton.plugin.js

--- js/toggleButton.plugin.js
+++ js/toggleButton.plugin.js
@@ -1,3 +1,56 @@
+// Polyfill for creating CustomEvents on IE9/10/11
+
+// code pulled from:
+// https://github.com/d4tocchini/customevent-polyfill
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
+
+(function() {
+  if (typeof window === 'undefined') {
+    return;
+  }
+
+  try {
+    var ce = new window.CustomEvent('test', { cancelable: true });
+    ce.preventDefault();
+    if (ce.defaultPrevented !== true) {
+      // IE has problems with .preventDefault() on custom events
+      // http://stackoverflow.com/questions/23349191
+      throw new Error('Could not prevent default');
+    }
+  } catch (e) {
+    var CustomEvent = function(event, params) {
+      var evt, origPrevent;
+      params = params || {};
+      params.bubbles = !!params.bubbles;
+      params.cancelable = !!params.cancelable;
+
+      evt = document.createEvent('CustomEvent');
+      evt.initCustomEvent(
+        event,
+        params.bubbles,
+        params.cancelable,
+        params.detail
+      );
+      origPrevent = evt.preventDefault;
+      evt.preventDefault = function() {
+        origPrevent.call(this);
+        try {
+          Object.defineProperty(this, 'defaultPrevented', {
+            get: function() {
+              return true;
+            }
+          });
+        } catch (e) {
+          this.defaultPrevented = true;
+        }
+      };
+      return evt;
+    };
+
+    CustomEvent.prototype = window.Event.prototype;
+    window.CustomEvent = CustomEvent; // expose definition to window
+  }
+})();
 /**
  * @author r00t80y<https://github.com/R00T80Y>
  * @file
@@ -133,7 +186,7 @@ function Plugin($rootElement, pluginOptions) {
   addEvents();
 
   // Вызываем событие change для флажков
-  $checkbox.dispatchEvent(new Event('change'));
+  $checkbox.dispatchEvent(new CustomEvent('change'));
 
   // Хук init
   Utils.type(pluginOptions.init) === 'function' && pluginOptions.init();
@@ -141,7 +194,7 @@ function Plugin($rootElement, pluginOptions) {
   // Изменяет состояние флажка на противоположное
   function toggleState() {
     $checkbox.checked = !$checkbox.checked;
-    $checkbox.dispatchEvent(new Event('change'));
+    $checkbox.dispatchEvent(new CustomEvent('change'));
   }
 
   // Событие: cостояние флажка изменилось
